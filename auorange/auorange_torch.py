5dac6f4c8b4ad17ff51b5020b7a18bf015bca121
KakaruHayate <97896816+KakaruHayate@users.noreply.github.com>
2024-05-20 22:49:42 +0800

    Update auorange_torch.py

--- auorange/auorange_torch.py
+++ auorange/auorange_torch.py
@@ -1,21 +1,18 @@
-# Copyright 2020 Yablon Ding
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#    http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import os
 import numpy as np
 import torch
 import torch.nn.functional as F
 from librosa.filters import mel as librosa_mel_fn
+
+
+class PreEmphasis(torch.nn.Module):
+    def __init__(self, coefficient: float = 0.9375):
+        super().__init__()
+        self.coefficient = coefficient
+        self.register_buffer('kernel', torch.tensor([-coefficient, 1.], dtype=torch.float32).unsqueeze(0).unsqueeze(0))
+
+    def forward(self, signal):
+        return F.conv1d(signal, self.kernel)
 
 
 class Audio2Mel(torch.nn.Module):
