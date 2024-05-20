bcbba0e7938c18f78feb740d86c746a70aaa7828
KakaruHayate <97896816+KakaruHayate@users.noreply.github.com>
2024-05-20 20:32:33 +0800

    Update torch_predict.py

--- torch_predict.py
+++ torch_predict.py
@@ -4,9 +4,9 @@ import numpy as np
 
 
 from auorange.utils import plot, plot_spec, load_wav, save_wav
-from auorange.auorange_torch import Audio2Mel,Mel2LPC, LPC2Wav, LPC2WavWithResidual
+from auorange.auorange_torch import Audio2Mel, Mel2LPC, LPC2Wav
 
-wav_name = 'wavs/2001000006.wav'
+wav_name = 'wavs/2006000193.wav'
 sample_rate = 44100
 n_fft = 2048
 num_mels = 128
@@ -15,7 +15,7 @@ win_length = 2048
 lpc_order = 8
 clip_lpc = True
 mel_fmin = 40
-f0 = 40.
+mel_fmax = 16000
 
 wav_data = load_wav(wav_name, sample_rate)
 wav_data = torch.tensor(wav_data).unsqueeze(0).unsqueeze(1)
@@ -28,7 +28,7 @@ a2w = Audio2Mel(
     n_fft=n_fft, 
     n_mel_channels=num_mels, 
     mel_fmin=mel_fmin, 
-    mel_fmax=None
+    mel_fmax=mel_fmax
     )
 mel = a2w(wav_data)
 
@@ -40,39 +40,33 @@ m2l = Mel2LPC(
     n_fft=n_fft, 
     n_mel_channels=num_mels, 
     mel_fmin=mel_fmin, 
-    mel_fmax=None, 
-    f0=f0, 
+    mel_fmax=mel_fmax, 
     lpc_order=lpc_order
     )
-LPC_ctrl = m2l(mel)
+LPC_ctrl_mel = m2l(mel.transpose(1, 2))
 
 
-l2w = LPC2Wav(lpc_order=lpc_order, clip_lpc=True)
-wav_pred = l2w(LPC_ctrl,wav_data)
+wav_pred_mel = LPC2Wav(LPC_ctrl_mel, wav_data, lpc_order=lpc_order, clip_lpc=clip_lpc)
+
 
 # Make sure the predicted audio and the original audio have the same shape
-if wav_pred.shape[2] > wav_data.shape[2]:
-    wav_pred = wav_pred[:, :, :wav_data.shape[2]]
-elif wav_pred.shape[2] < wav_data.shape[2]:
-    wav_data = wav_data[:, :, :wav_pred.shape[2]]
+if wav_pred_mel.shape[2] > wav_data.shape[2]:
+    wav_pred_mel = wav_pred_mel[:, :, :wav_data.shape[2]]
+elif wav_pred_mel.shape[2] < wav_data.shape[2]:
+    wav_data = wav_data[:, :, :wav_pred_mel.shape[2]]
+
 
 # Compute the residual
-residual = wav_data - wav_pred
-
-
-r2w = LPC2WavWithResidual(lpc_order=lpc_order, clip_lpc=True)
-error_pred = r2w(LPC_ctrl, residual)
+residual = wav_data - wav_pred_mel
 
 
 wav_data = wav_data.squeeze(0).squeeze(0).numpy()
-wav_pred = wav_pred.squeeze(0).squeeze(0).numpy()
-error_pred = error_pred.squeeze(0).squeeze(0).numpy()
+wav_pred_mel = wav_pred_mel.squeeze(0).squeeze(0).numpy()
 error = residual.squeeze(0).squeeze(0).numpy()
 
 
-save_wav(wav_pred, 'wavs/wav_pred2001000006.wav', sample_rate)
-save_wav(error, 'wavs/error2001000006.wav', sample_rate)
-save_wav(error_pred, 'wavs/error_pred2001000006.wav', sample_rate)
+save_wav(wav_pred_mel, 'wavs/wav_pred_mel2006000193.wav', sample_rate)
+save_wav(error, 'wavs/error2006000193.wav', sample_rate)
 
 
 fig = plt.figure(figsize=(30, 5))
@@ -81,9 +75,9 @@ plt.ylabel('wav_data')
 plt.xlabel('time')
 plt.plot(wav_data)
 plt.subplot(312)
-plt.ylabel('wav_pred')
+plt.ylabel('wav_pred_mel')
 plt.xlabel('time')
-plt.plot(wav_pred)
+plt.plot(wav_pred_mel)
 plt.subplot(313)
 plt.ylabel('error')
 plt.xlabel('time')
